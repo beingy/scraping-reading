cc03f340e8bf9515f4566908f83a0e4547f44141
beingy <beingy@gmail.com>
2016-06-03 20:56:23 -0400

    add scraper.rb code along

---
+++ scraper.rb
@@ -0,0 +1,24 @@
+require 'nokogiri'
+require 'open-uri'
+
+html1 = open("http://flatironschool.com")
+doc1 = Nokogiri::HTML(html1)
+puts doc1
+
+doc2 = Nokogiri::HTML(open("http://flatironschool.com"))
+puts doc2.css(".grey-text")
+# => <span style="color:#14b3d9;" class="grey-text">Master Web Development at Home</span>
+# => <span class="grey-text">Join our state-of-the-art online campus.</span>
+# => <span class="grey-text">Free Full-Access Trial.</span>
+
+puts doc2.xpath('//span[@style="color:#14b3d9;"]').text
+# => Master Web Development at Home
+
+html2 = open("http://flatironschool.com/team")
+doc3 = Nokogiri::HTML(html2)
+
+instructors = doc3.css("#instructors")
+# puts instructors.css("h2")[0].text
+instructors.css("h2").each do |i|
+    puts "Flatiron School <3 #{i.text}"
+end
