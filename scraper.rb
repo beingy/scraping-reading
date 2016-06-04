8c1b0431a1b05ebf4200d5a6844cdb2d0e1b8ca1
Being Young <beingy@gmail.com>
2016-06-04 06:22:18 -0400

    update scraper.rb parsing syntax

--- scraper.rb
+++ scraper.rb
@@ -1,17 +1,22 @@
+require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-html1 = open("http://flatironschool.com")
-doc1 = Nokogiri::HTML(html1)
-puts doc1
+# html1 = open("http://flatironschool.com")
+# doc1 = Nokogiri::HTML(html1)
+# puts doc1
 
-doc2 = Nokogiri::HTML(open("http://flatironschool.com"))
-puts doc2.css(".grey-text")
-# => <span style="color:#14b3d9;" class="grey-text">Master Web Development at Home</span>
-# => <span class="grey-text">Join our state-of-the-art online campus.</span>
-# => <span class="grey-text">Free Full-Access Trial.</span>
+# doc2 = Nokogiri::HTML(open("http://flatironschool.com"))
+# puts doc2.css(".grey-text")[0].text
+# puts doc2.css("span[style='color:#14b3d9;']").text
+# # => Master Web Development at Home 
 
-puts doc2.xpath('//span[@style="color:#14b3d9;"]').text
+# puts doc2.css(".grey-text")
+# # => <span style="color:#14b3d9;" class="grey-text">Master Web Development at Home</span>
+# # => <span class="grey-text">Join our state-of-the-art online campus.</span>
+# # => <span class="grey-text">Free Full-Access Trial.</span>
+
+# puts doc2.xpath('//span[@style="color:#14b3d9;"]').text
 # => Master Web Development at Home
 
 html2 = open("http://flatironschool.com/team")
